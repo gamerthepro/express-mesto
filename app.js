f13bdfed27355c86d9fb97aa11b1315f5e3f93bb
Artem Polonnikov <gamer.thepro@yandex.ru>
2021-05-03 14:24:36 +0300

    Создание схемы и модели

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const express = require('express');
-// Слушаем 3000 порт
-const { PORT = 3000 } = process.env;
+const mongoose = require('mongoose');
+const users = require('./routes/users');
+const cards = require('./routes/cards');
 
 const app = express();
 
-app.listen(PORT, () => {
-    // Если всё работает, консоль покажет, какой порт приложение слушает
-    console.log(`App listening on port ${PORT}`)
-})
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+    useFindAndModify: false
+});
+
+app.use('/users', users);
+app.use('/cards', cards);
+
+
+
+app.listen(3000);
