a10ebe4696e19807badcfafd4aa269bb91eadf75
Artem Polonnikov <gamer.thepro@yandex.ru>
2021-05-03 17:13:01 +0300

    Итоговый код проектной работы 13

--- app.js
+++ app.js
@@ -1,20 +1,35 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const users = require('./routes/users');
 const cards = require('./routes/cards');
 
+const { PORT = 3000 } = process.env;
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
   useCreateIndex: true,
-    useFindAndModify: false
+  useFindAndModify: false,
+});
+
+app.use((req, res, next) => {
+  req.user = {
+    _id: '609002955c0c75c0c2db8fde',
+  };
+
+  next();
 });
 
 app.use('/users', users);
 app.use('/cards', cards);
+app.use((req, res) => {
+  res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
+});
 
+app.listen(PORT, () => {
 
-
-app.listen(3000);
+});
