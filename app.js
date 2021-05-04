dad622a382c1feaed37a253e7d44eed7135a8ce9
Artem Polonnikov <gamer.thepro@yandex.ru>
2021-05-04 15:33:22 +0300

    ревью 1

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ const { PORT = 3000 } = process.env;
 const app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
