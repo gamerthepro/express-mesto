d04b45f2f1f299822a77b526d3c3278330d76979
Artem Polonnikov <gamer.thepro@yandex.ru>
2021-05-08 13:08:18 +0300

    1. Добавьте email и password к схеме пользователя

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const { isEmail } = require('validator');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -17,6 +18,22 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: [isEmail, 'invalid email'],
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
 });
+
+userSchema.path('avatar').validate((val) => {
+  const urlRegex = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-/]))?/;
+  return urlRegex.test(val);
+}, 'Invalid URL.');
 
 module.exports = mongoose.model('user', userSchema);
