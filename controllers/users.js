8946a0708cdb24ad1c2e51a23e2449a97a5175b7
Artem Polonnikov <gamer.thepro@yandex.ru>
2021-05-18 18:17:07 +0300

    errors

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,12 @@
 const User = require('../models/user');
 const { errorOutput } = require('../utils/utils');
 
+const { BadRequestError } = require('../errors/400-BadRequestError');
+const { NotFoundError } = require('../errors/404-NotFoundError');
+const { UnauthorizedError } = require('../errors/401-UnauthorizedError');
+const { InternalServerError } = require('../errors/500-InternalServerError');
+const { ConflictError } = require('../errors/409-ConflictError');
+
 module.exports.getUsers = (req, res) => {
   User.find({})
     .then((users) => res.send({ data: users }))
@@ -14,12 +20,44 @@ module.exports.getUser = (req, res) => {
     .catch((err) => errorOutput(err, res));
 };
 
-module.exports.createUser = (req, res) => {
-  const { name, about, avatar, email, password, } = req.body;
-  const hash = await bcrypt.hash(password, 10);
-  User.create({ name, about, avatar, email, password: hash, })
-    .then((user) => res.send({ data: user }))
-    .catch((err) => errorOutput(err, res));
+module.exports.createUser = (req, res, next) => {
+  const {
+    name, about, avatar, email,
+  } = req.body;
+
+  bcrypt.hash(req.body.password, 10)
+    .then((hash) => User.create({
+      name, about, avatar, email, password: hash,
+    }))
+    .then((user) => res
+      .status(200)
+      .send({ data: { _id: user._id, email: user.email } }))
+    .catch((err) => errorOutput(err, res))
+    .catch(next);
+};
+
+module.exports.login = (req, res, next) => {
+  const { email, password } = req.body;
+
+  return User.findUserByCredentials(email, password)
+    .then((user) => {
+      const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'super-secret', { expiresIn: '7d' });
+      const data = {
+        _id: user._id,
+        name: user.name,
+        about: user.about,
+        avatar: user.avatar,
+        email: user.email,
+      };
+      res
+        .status(200)
+        .send({ data, token });
+    })
+    .catch((err) => {
+      res
+        .status(401)
+        .send({ message: err.message });
+    });
 };
 
 module.exports.updateUserInfo = (req, res) => {
