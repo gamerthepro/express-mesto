40cb88a1b61b624c8bb6d2384591f70900200229
Artem Polonnikov <gamer.thepro@yandex.ru>
2021-05-18 19:46:53 +0300

    7. Защитите API авторизацией

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser');
 const users = require('./routes/users');
 const cards = require('./routes/cards');
 
+const { handleAuthorization } = require('./middlewares/auth');
+
 const { PORT = 3000 } = process.env;
 const app = express();
 
@@ -16,6 +18,7 @@ const {
 app.use(bodyParser.json());
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useUnifiedTopology: true,
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
@@ -32,12 +35,12 @@ app.use((req, res, next) => {
 app.use('/signin', login);
 app.use('/signup', createUser);
 
-app.use('/users', users);
-app.use('/cards', cards);
+app.use('/users', handleAuthorization, users);
+app.use('/cards', handleAuthorization, cards);
 app.use((req, res) => {
   res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
 });
 
 app.listen(PORT, () => {
-
+  console.log(`Server launched sucesfully! App listening on port: ${PORT}`);
 });
