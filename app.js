71e947d65b8671188ceb6f3faf91523ede0ad1f4
Artem Polonnikov <gamer.thepro@yandex.ru>
2021-05-21 09:32:52 +0300

    ревью 2

--- app.js
+++ app.js
@@ -31,14 +31,14 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 
 app.use(requestLogger);
 
-app.use('/signin', celebrate({
+app.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().min(2).max(30),
     password: Joi.string().required().min(8),
   }),
 }), login);
 
-app.use('/signup', celebrate({
+app.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
