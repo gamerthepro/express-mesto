3bb2d99062ca83dd41875a33eb37d41c756b1ca4
Artem Polonnikov <gamer.thepro@yandex.ru>
2021-05-22 10:19:30 +0300

    ревью 3

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const { celebrate, Joi } = require('celebrate');
 const { errors } = require('celebrate');
 const users = require('./routes/users');
@@ -18,7 +19,7 @@ const {
 const { PORT = 3000 } = process.env;
 const app = express();
 
-app.use(bodyParser.json());
+app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
